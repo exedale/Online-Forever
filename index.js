28ca73cafb4f440a798940070b2405e0509efcc9
AWCY_V3NDES <91440921+exedale@users.noreply.github.com>
2025-06-15 15:50:00 +0800

    Final index.js

--- index.js
+++ index.js
@@ -6,15 +6,6 @@ const bot = new Eris(process.env.token);
 
 bot.on("error", (err) => {
   console.error(err); // or your preferred logger
-
-  bot.editStatus("online", {
-    name: "My roles will guide you",
-    type: 0 // 0 = Playing, 1 = Streaming, 2 = Listening, 3 = Watching, 5 = Competing
-  });
 });
 
-bot.on("error", (err) => {
-  console.error(err); // or your preferred logger
-});
-
-bot.connect();
+bot.connect(); // Get the bot to connect to Discord
