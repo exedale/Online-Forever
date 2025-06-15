2feaf4e383930518adbbf14c90134a441ee301c3
AWCY_V3NDES <91440921+exedale@users.noreply.github.com>
2025-06-15 15:45:19 +0800

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ bot.on("error", (err) => {
 
   bot.editStatus("online", {
     name: "My roles will guide you",
-    type: 5 // 0 = Playing, 1 = Streaming, 2 = Listening, 3 = Watching, 5 = Competing
+    type: 0 // 0 = Playing, 1 = Streaming, 2 = Listening, 3 = Watching, 5 = Competing
   });
 });
 
